4e3e1f74bdc480b2cadf6376ec93b2588f2bad63
James Vaughan <vaughanjames@microsoft.com>
2023-09-26 10:11:01 +0100

    update the welcome message

--- src/features/chat/chat-ui/start-new-chat.tsx
+++ src/features/chat/chat-ui/start-new-chat.tsx
@@ -19,11 +19,14 @@ export const StartNewChat: FC<Prop> = (props) => {
         </Typography>
         <div className="flex flex-col gap-2">
           <p className="">
-            Welcome to {AI_NAME}. You should interact in a friendly manner with
-            the AI assistant and refrain from participating in any harmful
-            activities.
+            Welcome to {AI_NAME}.
+            </p>
+          <p>
+             This is app is running in public cloud. Please do not submit any sensitive information. 
+            </p>
+          <p>
+            You can start a new chat by clicking the button below.
           </p>
-          <p>You can start a new chat with me by clicking the button below.</p>
         </div>
         <div className="-mx-5 -mb-5 p-5 flex flex-col border-t bg-muted">
           <NewChat />
